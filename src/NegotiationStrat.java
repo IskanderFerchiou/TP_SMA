170af41fac654d792b47f148ab379eb101d6192d
Iskander <iskander.ferchiou@etu.univ-lyon1.fr>
2023-01-29 15:08:21 +0100

    comment removed

--- src/NegotiationStrat.java
+++ src/NegotiationStrat.java
@@ -1,7 +1,4 @@
 public enum NegotiationStrat {
-    // plus la date limite de vente du ticket approche, plus le vendeur va essayer
-    // de diminuer le prix pour que le ticket soit vendu le plus rapidement possible.
-    // Inversement, s’il reste beaucoup de temps, le vendeur va essayer de maximiser son gain.
     DEFAULT,
     REMAINING_TIME,
     TICKETS_SIMILARITY
